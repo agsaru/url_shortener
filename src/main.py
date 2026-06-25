9e18ef90b00b8fcc5a48d54c8d6c151c289cc647
agsaru <aghifdgamer1101@gmail.com>
2026-06-25 05:41:52 +0000

    Integrated alembic

--- src/main.py
+++ src/main.py
@@ -3,8 +3,6 @@ from configs.db import engine, Base
 from models.model import Url 
 from routes.routes import router
 
-Base.metadata.create_all(bind=engine)
-
 app = FastAPI(title="URL Shortener")
 app.include_router(router)
 @app.get("/")
